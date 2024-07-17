e7714c15293f86d3ed8bc8117fd064706b192a3d
Krishna prasad Racherla <racherlakrishnaprasad4241@gmail.com>
2024-07-17 14:25:57 +0530

    Add files via upload

---
+++ PROJECT 1/script.js
@@ -0,0 +1,75 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search');
+const weatherbox = document.querySelector('.weather-box');
+const weatherdetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+document.addEventListener('click', () => {
+    const apikey = '0db3b9595d6b866969726bfec647999d';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '') {
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('City not found');
+            }
+            return response.json();
+        })
+        .then(data => {
+            container.style.height = '600px';
+            weatherbox.classList.add('active');
+            weatherdetails.classList.add('active');
+            error404.classList.remove('active');
+
+            
+            const image = document.querySelector('.weather-box img');
+            switch (data.weather[0].main.toLowerCase()) {
+                case 'clear':
+                    image.src = "images/haze.png";
+                    break;
+                case 'rain':
+                    image.src = "images/rainy.png";
+                    break;
+                case 'snow':
+                    image.src = "images/snow.png";
+                    break;
+                case 'clouds':
+                    image.src = 'images/clouds.png';
+                    break;
+                    case 'haze':
+                    image.src ='images/haze.png';
+                    break;
+                case 'thunderstorm':
+                    image.src = 'images/storm.png';
+                    break;
+                case 'mist':
+                    image.src ='images/mist.png';
+                    break;
+
+                default:
+                    image.src = 'images/clouds.png';
+                    break;
+            }
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            temperature.textContent = `${Math.round(data.main.temp)}°C`;
+            description.textContent = data.weather[0].description;
+
+            
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+            humidity.textContent = `${data.main.humidity}%`;
+            wind.textContent = `${data.wind.speed} km/h`;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            container.style.height = '400px';
+            weatherbox.classList.remove('active');
+            weatherdetails.classList.remove('active');
+            error404.classList.add('active');
+        });
+});
